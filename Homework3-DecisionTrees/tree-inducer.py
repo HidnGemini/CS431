bb26ecd2ca0a35f93e2f13e201ed4d528fbf3c9a
hidngemini <hidngemini@gmail.com>
2025-03-28 14:51:36 -0700

    [3] no pruning but working

--- Homework3-DecisionTrees/tree-inducer.py
+++ Homework3-DecisionTrees/tree-inducer.py
@@ -2,8 +2,16 @@ import csv
 import math
 
 
+"""
+This classifier is pretty good but I came up with a better one:
+Are you Stupid?:
+    +R
+    -D
+    .R
+"""
+
 def readData(path: str) -> list:
-    repSet = []
+    repSet = [] 
     with open(path, 'r') as file:
         reader = csv.reader(file, delimiter='\t')
         for row in reader:
@@ -16,6 +24,17 @@ def convertToDict(repSet: list) -> dict:
     for rep in repSet:
         dict[rep[0]] = (rep[1], rep[2])
     return dict
+
+
+def splitIntoTrainingAndTuning(repSet: list) -> tuple:
+    train = []
+    tune = []
+    for i in range(len(repSet)):
+        if i % 4 == 0:
+            tune.append(repSet[i])
+        else:
+            train.append(repSet[i])
+    return (train, tune)
 
 
 def countReps(repSet: list) -> tuple:
@@ -36,9 +55,18 @@ def countReps(repSet: list) -> tuple:
 def calculateEntropy(repSet: list) -> float:
     numDemocrats, numRepublicans = countReps(repSet)
     totalReps = numDemocrats + numRepublicans
-    entropy = \
-        -1 * (numDemocrats/totalReps) * math.log2((numDemocrats/totalReps)) + \
-        -1 * (numRepublicans/totalReps) * math.log2((numRepublicans/totalReps))
+    if numDemocrats != 0 and numRepublicans != 0:
+        entropy = \
+            -1 * (numDemocrats/totalReps) * math.log2((numDemocrats/totalReps)) + \
+            -1 * (numRepublicans/totalReps) * math.log2((numRepublicans/totalReps))
+    elif numRepublicans != 0:
+        entropy = \
+            -1 * (numRepublicans/totalReps) * math.log2((numRepublicans/totalReps))
+    elif numDemocrats != 0:
+        entropy = \
+            -1 * (numDemocrats/totalReps) * math.log2((numDemocrats/totalReps))
+    else:
+        entropy = 0
     return entropy
 
 
@@ -58,19 +86,104 @@ def seperateIntoSubGroups(repSet: list, issue: int) -> tuple:
     return (yae, nay, abstain)
 
 
-def clipIssue(repSet: list, issue: int) -> list:
-    # TODO: clip issue function to remove the issue at given index from all reps
-    pass 
+def calculateGain(group: list, subGroups: tuple) -> float:
+    preEntropy = calculateEntropy(group)
+    postEntropy = 0;
+    for subGroup in subGroups:
+        postEntropy += len(subGroup)/len(group)*(calculateEntropy(subGroup))
+    return preEntropy-postEntropy
+
+
+def findBestSplit(repSet: list, issues: str) -> int:
+    bestIssue = -1
+    bestGain = -1
+    for issue in issues:
+        issue = int(issue)
+        subgroups = seperateIntoSubGroups(repSet, issue)
+        gain = calculateGain(repSet, subgroups)
+        if gain > bestGain:
+            bestGain = gain
+            bestIssue = issue
+    return bestIssue
+
+
+def clipIndexFromString(string: str, index: int) -> str:
+    return string[:index]+string[index+1:]
+
+
+def printIIndents(i: int):
+    for j in range(i):
+        print("     ", end="")
+
+
+def induceTree(trainingSet: list, depth: int, issues: str, outcome: int, parentMajority: str):
+    dems, reps = countReps(trainingSet)
+    if (len(trainingSet) == 0):
+        printIIndents(depth)
+        print(f"{groupOrder[outcome]} ", end="")
+        print(parentMajority)
+        return
+    if dems == 0:
+        printIIndents(depth)
+        print(f"{groupOrder[outcome]} ", end="")
+        print("R")
+        return
+    if reps == 0:
+        printIIndents(depth)
+        print(f"{groupOrder[outcome]} ", end="")
+        print("D")
+        return
+
+    bestIssue = findBestSplit(trainingSet, issues)
+    newGroups = seperateIntoSubGroups(trainingSet, bestIssue)
+
+    if calculateGain(trainingSet, newGroups) == 0:
+        allSame = True
+        record = trainingSet[0][2]
+        for rep in trainingSet:
+            if record != rep[2]:
+                allSame = False
+                break;
+        if allSame:
+            printIIndents(depth)
+            print(f"{groupOrder[outcome]} ", end="")
+            symbol = parentMajority+"**" if reps == dems else "R*" if reps > dems else "D*"
+            print(symbol)
+            return
+
+
+    printIIndents(depth)
+    print(f"{groupOrder[outcome]} ", end="")
+    print(f"Issue {chr(bestIssue+ord('A'))}:")
+    #print(f"Issue {bestIssue}")
+    issues = clipIndexFromString(issues, issues.index(str(bestIssue)))
+    majority = parentMajority if dems == reps else "D" if dems > reps else "R"
+    for i,group in enumerate(newGroups):
+
+
+        # recursive call
+        induceTree(group, depth+1, issues, i, majority)
 
 
 if __name__ == "__main__":
     reps = readData("Homework3-DecisionTrees/voting-data.tsv")
+    groupOrder = ("+", "-", ".", "")
 
-    entropy = calculateEntropy(reps)
-    print(f"info entropy: {entropy}")
+    train, tune = splitIntoTrainingAndTuning(reps)
 
-    yae, nae, abstain = seperateIntoSubGroups(reps, 0)
-    print(abstain)
+    # bestIssue = findBestSplit(train, issues)
+    # print(f"Issue {chr(bestIssue+ord('A'))}:")
+    # newGroups = seperateIntoSubGroups(train, bestIssue)
+    # issues = clipIndexFromString(issues, bestIssue)
+    # for i, group in enumerate(newGroups):
+    #     print(f"    {groupOrder[i]} ", end="")
+    #     bestIssue = findBestSplit(group, issues)
+    #     print(f"Issue {chr(bestIssue+ord('A'))}:")
+    induceTree(train, 0, "0123456789", 3, None)
 
-    a_entropy = calculateEntropy(abstain)
-    print(a_entropy)
+    # string = "0123456789"
+    # print(string)
+    # string = clipIndexFromString(string, 2)
+    # print(string)
+
+    
